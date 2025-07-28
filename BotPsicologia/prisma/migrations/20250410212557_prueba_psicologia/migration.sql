e405774939cc5deef5875b802a508ace6cfdd71d
jhonbase <92900691+jhonbase@users.noreply.github.com>
2025-07-28 16:33:39 -0500

    Delete BotPsicologia directory

--- BotPsicologia/prisma/migrations/20250410212557_prueba_psicologia/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `informacionusuario` MODIFY `motivo` VARCHAR(191) NULL DEFAULT '';
--- BotPsicologia/prisma/migrations/20250410213111_a/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `informacionusuario` MODIFY `motivo` TEXT NULL;
